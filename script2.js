f54ca1f1645a94fffed48df1c0a22b0c300fb17c
anujyadav1783 <156939695+anujyadav1783@users.noreply.github.com>
2024-05-12 17:16:34 +0530

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,35 @@
+var typed = new Typed(".text",{
+    strings: ["Designing and coding the user interface (UI) and user experience (UX) of websites and web applications using HTML, CSS, JS, etc. "],
+    typeSpeed: 50,
+    loop: true
+})
+
+var typed = new Typed(".text1",{
+    strings: ["Process of creating, building, and maintaining websites and web applications including aspects such as web design, web publishing, web programming,etc."],
+    typeSpeed: 50,
+    loop: true
+})
+
+var typed = new Typed(".text2",{
+    strings: ["Primary goal is to create designs that are visually appealing and communicate the intended message effectively using software tools."],
+    typeSpeed: 50,
+    loop: true
+})
+
+var typed = new Typed(".text3",{
+    strings: ["A well-designed mobile app can help businesses reach more customers, improve marketing strategies, provide value to the customers, increase brand awareness, etc."],
+    typeSpeed: 50,
+    loop: true
+})
+
+var typed = new Typed(".text4",{
+    strings: ["Software application that allows users to create, edit, and manipulate graphical content using a graphical user interface (GUI) instead of text commands."],
+    typeSpeed: 50,
+    loop: true
+})
+
+var typed = new Typed(".text5",{
+    strings: ["The art or practice of taking and processing photographs.</"],
+    typeSpeed: 50,
+    loop: true
+})
