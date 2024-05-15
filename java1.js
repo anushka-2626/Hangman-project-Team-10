8316288883a15b32391b00d102b4e2157133504d
Anubhav <159409040+Anubhav20005@users.noreply.github.com>
2024-05-15 19:45:20 +0530

    Added js files

---
+++ java1.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Date of Birth:26 March 2000","Address:South Urban, Mumbai"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
